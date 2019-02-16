78c51df99f4cb6c266ff2d454e82659286142751
xinxiamu <932852117@qq.com>
2019-02-16 18:19:12 +0800

    修改

--- src/main/java/com/xrlj/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/xrlj/eurekaserver/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class XrljEurekaServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XrljEurekaServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
