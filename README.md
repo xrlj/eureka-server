6784c6f07b2b51d44f064e0d96ac179a1e67b090
xinxiamu <932852117@qq.com>
2019-07-02 20:02:11 +0800

    重构

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # xrlj-eureka-server
+
+https://spring.io/guides/gs/spring-boot-docker/
+
+## 执行
+
+    $ docker run -e "SPRING_PROFILES_ACTIVE=prod" -p 8080:8080 -t springio/gs-spring-boot-docker
+    
+    ----------------------
+    
+    docker run --restart=always --name eureka-server-test -e "SPRING_PROFILES_ACTIVE=test" -p 1111:1111 -t 120.79.2.30:8082/xrlj/eureka-server:0.0.1
+    
+    ------------------------------------
+    
+    docker run --restart=always --name eureka-server-test -e "SPRING_PROFILES_ACTIVE=test" -e "EUREKA_INSTANCE_IP-ADDRESS=ip" -p 1111:1111 -d ip:8082/xrlj/eureka-server:0.0.1
--- src/main/java/com/xrlj/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/xrlj/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.xrlj.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
