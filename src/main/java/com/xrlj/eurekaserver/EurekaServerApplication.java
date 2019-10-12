f8521e45ad561d66f769eccfb230dbb9267a2619
xinxiamu <932852117@qq.com>
2019-10-12 15:40:45 +0800

    重构

--- src/main/java/com/xrlj/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/xrlj/eurekaserver/EurekaServerApplication.java
@@ -1,5 +1,7 @@
 package com.xrlj.eurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
@@ -10,8 +12,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class EurekaServerApplication {
 
+	private final static Logger log = LoggerFactory.getLogger(EurekaServerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+		log.info(">>>>>服务{}启动成功：{}", EurekaServerApplication.class.getSimpleName(), args);
 	}
 
 }
